fda32a64bf8a946c2e543c04b77749357a0a2cc7
Codeaholic10 <aadi.tidy99@gmail.com>
2026-02-21 13:02:16 +0530

    Model Training Done

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -1,4 +1,4 @@
-import os
+﻿import os
 import sys
 from src.exception import CustomException
 from src.logger import logging
@@ -7,11 +7,15 @@ from sklearn.model_selection import train_test_split
 from dataclasses import dataclass
 from src.components.data_transformation import DataTransformation
 from src.components.data_transformation import DataTransformationConfig
+from src.components.model_trainer import ModelTrainer
+
+
 @dataclass
 class DataIngestionConfig:
-    train_data_path: str=os.path.join('artifacts','train.csv')
-    test_data_path: str=os.path.join('artifacts','test.csv')
-    raw_data_path: str=os.path.join('artifacts','data.csv')
+    train_data_path: str = os.path.join('artifacts', 'train.csv')
+    test_data_path: str = os.path.join('artifacts', 'test.csv')
+    raw_data_path: str = os.path.join('artifacts', 'data.csv')
+
 
 class DataIngestion:
     def __init__(self):
@@ -20,7 +24,7 @@ class DataIngestion:
     def initiate_data_ingestion(self):
         logging.info("Entered the data ingestion method or component")
         try:
-            df = pd.read_csv(os.path.join('notebook\data\stud.csv'))
+            df = pd.read_csv(os.path.join('notebook', 'data', 'stud.csv'))
             logging.info("Read the dataset as dataframe")
 
             os.makedirs(os.path.dirname(self.ingestion_config.train_data_path), exist_ok=True)
@@ -41,9 +45,15 @@ class DataIngestion:
             )
         except Exception as e:
             raise CustomException(e, sys)
+
+
 if __name__ == "__main__":
     obj = DataIngestion()
-    train_data,test_data = obj.initiate_data_ingestion()
+    train_data, test_data = obj.initiate_data_ingestion()
 
     data_transformation = DataTransformation()
-    data_transformation.initiate_data_transformation(train_data,test_data)
+    train_arr, test_arr, _ = data_transformation.initiate_data_transformation(train_data, test_data)
+
+    model_trainer = ModelTrainer()
+    r2 = model_trainer.initiate_model_trainer(train_arr, test_arr)
+    print(f"Model training completed. R2 score: {r2}")
